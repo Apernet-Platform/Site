e7f82416db083da2c5bf1b299b3744f8bcbb0427
Pigioty <Pigiotyreal@gmail.com>
2022-12-31 17:52:15 -0500

    Connected to DB

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const port = 4000
+
+require("dotenv").config()
+
+//DB
+
+mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on("error", console.error.bind(console, "DB Connection error"))
+db.once("open", function() {
+    console.log("Connected to MongoDB!")
+})
+
+//Routes
 
 app.set("view engine", "ejs")
 
